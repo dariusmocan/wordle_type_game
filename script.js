d9663f9db10f28050e39b15aeecc0f05eb146d72
dariusmocan <dariusmocan2003@gmail.com>
2025-12-25 19:22:16 +0200

    added game over. cleaned up code

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+// word to be guessed
 let word = "caine";
+
+// stores letter appearances of the original word
+const letterCounts = {};
+for (const letter of word){
+    letterCounts[letter] = (letterCounts[letter] || 0) + 1;
+}
 
 const table = document.getElementById("table");
 const rows = 6;
 const cols = word.length;
+let game_over = false;
 
+// initiating table with 6 tries of word.length cols
 for (let i = 0; i < 6; i++) {
   const row = document.createElement("div");
   row.className = "row";
@@ -16,52 +25,67 @@ for (let i = 0; i < 6; i++) {
   table.appendChild(row);
 }
 
-let currenRow = 0,
+let currentRow = 0,
   currentCol = 0,
   currentWord = "";
 
+// keydown - typing letter for guesses
 document.addEventListener("keydown", (e) => {
+  // if game over - no other modifications
+  if (game_over){
+    return;
+  }
+  
   if (e.key.length === 1 && e.key.match(/[a-z]/i)) {
+    // cell gets the key value if not out of cols
     if (currentCol < cols) {
-      const cell = table.children[currenRow].children[currentCol];
+      const cell = table.children[currentRow].children[currentCol];
       cell.textContent = e.key.toUpperCase();
       currentCol++;
 
       currentWord += e.key;
     }
+    // delete cell value if we have anything
   } else if (e.key === "Backspace") {
     if (currentCol > 0) {
       currentCol--;
-      const cell = table.children[currenRow].children[currentCol];
+      const cell = table.children[currentRow].children[currentCol];
       cell.textContent = "";
 
       currentWord.slice(0, currentCol);
     }
+    // on enter check the letters
   } else if (e.key === "Enter") {
+    // if not enough letters => alert
     if (currentWord.length < word.length) {
       alert("incorrect length");
     } else {
-      const rowCells = table.children[currenRow].children;    
-
+      const rowCells = table.children[currentRow].children;    
+    
       if (isRight(currentWord)) {
         for (const c of rowCells){
             c.classList.add('true_word');
         }
+        // game over!
+        setTimeout(() =>{
+            alert('you won!');
+        }, 900);
+        game_over = true;
       } else {
-        const letterCount = {};
-        for (const letter of word){
-            letterCount[letter] = (letterCount[letter] || 0) + 1;
-        }
+        // copy letter appearances of original word
+        const letterCount = {...letterCounts};
 
+        // perfect guesses have priority
         for (let i = 0; i < cols; i++){
             const guessedLetter = rowCells[i].textContent.toLowerCase(); 
             if ( guessedLetter === word[i]){
                 rowCells[i].classList.add('true_word');
+                // decrease letter apprearances
                 letterCount[guessedLetter]--;
             }
         }
 
-
+        // for partial guesses we check if there are any other appearances before
         for (let i = 0; i < cols; i ++){
             const guessedLetter = rowCells[i].textContent.toLowerCase(); 
             if (guessedLetter === word[i]){
@@ -76,12 +100,20 @@ document.addEventListener("keydown", (e) => {
         }
 
         setTimeout(() => {
-            currenRow++;
+            currentRow++;
             currentCol = 0;
             currentWord = '';
+
+            if (currentRow >= rows) {
+                game_over = true;
+
+                setTimeout( () => {
+                    alert('better luck next time.. :((');
+                }, 500);
+              }
         },400);
 
-        //console.log(currentWord);
+        //console.log('current word : ' + currentWord + ' ; current row : ' + currentRow);
       }
     }
   }
